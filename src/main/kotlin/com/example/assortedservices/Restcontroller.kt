5d6ef733f925798d2369f0a3f59dbd736bbbe0e0
hp <hp@nextgentel.com>
2022-08-30 19:11:51 +0200

    -la til random.html

--- src/main/kotlin/com/example/assortedservices/Restcontroller.kt
+++ src/main/kotlin/com/example/assortedservices/Restcontroller.kt
@@ -1,12 +1,21 @@
 package com.example.assortedservices
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import kotlin.random.Random
 
+@RestController
 class Restcontroller {
 
     @GetMapping("/")
     fun defultReplay():String {
         return "Hello Webhuset"
+    }
+
+    @GetMapping ("random")
+    fun getRandomNumber():String {
+        var randomNumber = Random.nextInt(0,1000)
+        return randomNumber.toString()
     }
 
 }
